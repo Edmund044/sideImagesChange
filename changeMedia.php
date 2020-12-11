adb1aa71eefeaca1f458eecead6d9f4a48ba2080
Edmund044 <edmundopiyo@gmail.com>
2020-12-11 17:34:15 +0300

    Create changeMedia.php

---
+++ changeMedia.php
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+   function Choose(id){
+    var media = id.getAttribute("src");
+     console.log(media);
+     document.getElementById("myimage").setAttribute("src", media);
+     
+     //pictures[index].style.opacity = "1";
+    }
+</script>
